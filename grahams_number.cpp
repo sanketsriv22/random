db5c3ddae583174588832ab11256002c07a8e6d1
Sanket Srivastava <sanketsriv22@gmail.com>
2025-10-18 14:19:25 -0400

    modular exponentiation works

--- grahams_number.cpp
+++ grahams_number.cpp
@@ -2,11 +2,20 @@
 #include <cmath>
 using namespace std;
 
-
-long long mod_pow(long base, long exp, long mod)
+// using modular exponentiation
+long long mod_pow(long long base, long long exp, long long mod)
 {
-    // using proof that f(n+1) = f(n) (mod 10^n)
-    long long result;
+    // using proof that f(n+1) = f(n) (mod 10^n), where f(n) = base^exp
+    // identity below for memory efficiency
+    // (a*b) mod(m) = [a mod(m) * b mod(m)] mod(m)
+    
+    long long result = 1;
+
+    for (int i=1; i<=exp; i++)
+    {
+        result = (base * result) % mod;
+    }
+
     return result;
 }
 
@@ -20,8 +29,14 @@ long long graham_last_digits(int digits)
 int main()
 {
     int digits;
+    
     cout << "how many digits: ";
     cin >> digits;
+
+    long long mod = digits;//pow(10, digits);
+    cout << "mod = " << mod << endl;
+    cout << "mod_pow of 4, 13 = " << mod_pow(4, 13, mod) << endl;
+
     cout << "last " << digits << " of grahams number: " << graham_last_digits(digits) << endl;
     return 0;
     
